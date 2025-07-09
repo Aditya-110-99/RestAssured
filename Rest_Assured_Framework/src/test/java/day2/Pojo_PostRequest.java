882224ac67ce84ea5a557723372a409ffce07c01
Aditya <Adityacroxtron1@gmail.com>
2025-07-09 16:04:49 +0530

    updated PostRequest & added TestData Directory

---
+++ Rest_Assured_Framework/src/test/java/day2/Pojo_PostRequest.java
@@ -0,0 +1,55 @@
+package day2;
+
+public class Pojo_PostRequest {
+    String name;
+    String gender;
+    int physics;
+    int maths;
+    int english;
+
+    public int getEnglish() {
+        return english;
+    }
+
+    public void setEnglish(int english) {
+        this.english = english;
+    }
+
+    public int getMaths() {
+        return maths;
+    }
+
+    public void setMaths(int maths) {
+        this.maths = maths;
+    }
+
+    public int getPhysics() {
+        return physics;
+    }
+
+    public void setPhysics(int physics) {
+        this.physics = physics;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+
+
+
+}
--- Rest_Assured_Framework/src/test/java/day2/PostRequests.java
+++ Rest_Assured_Framework/src/test/java/day2/PostRequests.java
@@ -1,7 +1,12 @@
 package day2;
 
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.FileReader;
 import java.util.HashMap;
 
 import static io.restassured.RestAssured.*;
@@ -9,7 +14,7 @@ import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 public class PostRequests {
-
+    String id;
 
 //    //Using HashMap
 //    @Test
@@ -41,29 +46,81 @@ public class PostRequests {
 //    }
 
     //Using org.json library
+//    @Test
+//    void testJsonLibrary() {
+//        JSONObject data = new JSONObject();
+//        data.put("name", "Aditya");
+//        data.put("gender", "Male");
+//        data.put("physics", 55);
+//        data.put("maths", 97);
+//        data.put("english", 77);
+//
+//      Response response =  given().contentType("application/json")
+//                .body(data.toString())
+//                .when().post("http://localhost:3000/students");
+//               id = response.jsonPath().getString("id");
+//
+//                response.then().statusCode(201)
+//                .body("name", equalTo("Aditya"))
+//                .body("gender", equalTo("Male"))
+//                .body("physics", equalTo(55))
+//                .body("maths", equalTo(97))
+//                .body("english", equalTo(77))
+//                .header("Content-Type", "application/json")
+//                .log().all();
+//
+//
+//    }
+
+
+    //using POJO
+//    @Test
+//    void usingPojo() {
+//        Pojo_PostRequest data = new Pojo_PostRequest();
+//        data.setName("Riya");
+//        data.setGender("Female");
+//        data.setPhysics(55);
+//        data.setMaths(97);
+//        data.setEnglish(77);
+//
+//
+//        Response response = given().contentType("application/json")
+//                .body(data)
+//                .when().post("http://localhost:3000/students");
+//        id = response.jsonPath().getString("id");
+//
+//        response.then().statusCode(201)
+//                .body("name", equalTo("Riya"))
+//                .body("gender", equalTo("Female"))
+//                .body("physics", equalTo(55))
+//                .body("maths", equalTo(97))
+//                .body("english", equalTo(77))
+//                .header("Content-Type", "application/json")
+//                .log().all();
+//
+//    }
+
+    //using External Json File
     @Test
-    void testJsonLibrary() {
-        HashMap data = new HashMap();
-        data.put("name", "Aditya");
-        data.put("gender", "Male");
-        data.put("physics", 55);
-        data.put("maths", 97);
-        data.put("english", 77);
+    void usingExternalJsonFile() throws Exception
+    {
+        File file = new File("TestData/body.json");
+        FileReader fr = new FileReader(file);
+        JSONTokener jt = new JSONTokener(fr);
+        JSONObject data = new JSONObject(jt);
 
-        given().contentType("application/json")
-                .body(data)
-                .when().post("http://localhost:3000/students")
+        Response response = given().contentType("application/json")
+                .body(data.toString())
+                .when().post("http://localhost:3000/students");
+        id = response.jsonPath().getString("id");
 
-
-                .then().statusCode(201)
-                .body("name", equalTo("Aditya"))
+        response.then().statusCode(201)
+                .body("name", equalTo("arun"))
                 .body("gender", equalTo("Male"))
-                .body("physics", equalTo(55))
-                .body("maths", equalTo(97))
-                .body("english", equalTo(77))
+                .body("physics", equalTo(88))
+                .body("maths", equalTo(87))
+                .body("english", equalTo(78))
                 .header("Content-Type", "application/json")
-
-
                 .log().all();
 
 
@@ -72,26 +129,26 @@ public class PostRequests {
 
     @Test
     void createStudent() {
-        HashMap data = new HashMap();
-        data.put("name", "Riya");
-        data.put("gender", "Female");
-        data.put("physics", 45);
-        data.put("maths", 91);
-        data.put("english", 17);
+
+        JSONObject data = new JSONObject();
+        data.put("name", "Aditya");
+        data.put("gender", "Male");
+        data.put("physics", 55);
+        data.put("maths", 97);
+        data.put("english", 77);
+
         given().contentType("application/json")
                 .body(data)
                 .when().get("http://localhost:3000/students")
-
                 .then().statusCode(200)
-
                 .log().all();
     }
 
 
-    @Test(priority = 2, dependsOnMethods = {"testPostHashMap"})
+    @Test(priority = 2, dependsOnMethods = {"usingExternalJsonFile"})
     void testDelete() {
         given()
-                .when().delete("http://localhost:3000/students/2")
+                .when().delete("http://localhost:3000/students/" + id)
                 .then().statusCode(200).log().all();
 
     }
