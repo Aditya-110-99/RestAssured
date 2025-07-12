1e16abfb4cffda0d2d0b6a82199b78cc20d91777
Aditya <Adityacroxtron1@gmail.com>
2025-07-12 13:47:53 +0530

    added Headers and Logging

--- Rest_Assured_Framework/src/test/java/day3/LoggingDemo.java
+++ Rest_Assured_Framework/src/test/java/day3/LoggingDemo.java
@@ -1,5 +1,26 @@
 package day3;
 
-public class LoggingDemo
-{
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class LoggingDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(LoggingDemo.class);
+
+    @Test
+    void testLogs() {
+        given()
+                .when().get("https://www.google.com/")
+                .then().log().cookies()
+//                .log().body()
+//                .log().headers()
+                    ;
+
+    }
+
 }
